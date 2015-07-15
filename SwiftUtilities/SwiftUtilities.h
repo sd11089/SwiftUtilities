f62dc325bf5eb79ccc881d62e98b72a0a4414087
Stephen Donnell <sdonnell.dev@gmail.com>
2015-07-15 14:41:46 -0400

    Initial Commit

---
+++ SwiftUtilities/SwiftUtilities.h
@@ -0,0 +1,19 @@
+//
+//  SwiftUtilities.h
+//  SwiftUtilities
+//
+//  Created by Stephen Donnell on 7/15/15.
+//  Copyright (c) 2015 Stephen Donnell. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftUtilities.
+FOUNDATION_EXPORT double SwiftUtilitiesVersionNumber;
+
+//! Project version string for SwiftUtilities.
+FOUNDATION_EXPORT const unsigned char SwiftUtilitiesVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftUtilities/PublicHeader.h>
+
+
